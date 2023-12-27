053a8fed97d21122203a8c27095604dd17f39b11
Kiran S G <gskiran526@gmail.com>
2023-12-28 02:17:22 +0530

    FAQ

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import Hero from "../../components/Hero/Hero";
 import Section from "../../components/Section/Section";
 import styles from "./HomePage.module.css";
 import {fetchFilters} from "../../api/api";
+import FaqSection from "../../components/FaqSection/FaqSection";
 
 function HomePage() {
    const { data } = useOutletContext();
@@ -14,8 +15,12 @@ function HomePage() {
         <div className={styles.wrapper}>
             <Section title="Top Albums" data={topAlbums} type="album" />
             <Section title="New Albums" data={newAlbums} type="album" />
+            <hr style={{ backgroundColor: "#34C94B", height: "2px", border: "none" }} />
             <Section title="Songs" data={songs} filterSource={fetchFilters} type="song" />
+            <hr style={{ backgroundColor: "#34C94B", height: "2px", border: "none" }} />
         </div>
+       <FaqSection/>
+
     </>
    )
 }
